0ec01dad69fe499fe7cb85bea70ade0dd2f813cd
attendance1978-wq <attendance1978@gmail.com>
2026-01-28 15:58:37 +0800

    Update triangle.js

--- src/libs/triangle.js
+++ src/libs/triangle.js
@@ -1,1 +1,10 @@
+import { Vec3, Mesh } from './core.js'
 
+export function Triangle(size=1){
+  const v=[
+    new Vec3(0,size,0),
+    new Vec3(-size,-size,0),
+    new Vec3(size,-size,0)
+  ]
+  return new Mesh(v,[[0,1],[1,2],[2,0]])
+}
